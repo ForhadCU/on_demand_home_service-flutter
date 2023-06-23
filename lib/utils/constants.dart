0d2848920902c3173ead9e18a584b2ec3d00d7a6
ForhadCU <kashem.tipo@gmail.com>
2023-06-23 15:48:42 +0600

    Just Up

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,3 +1,5 @@
 class MyConstants {
-  static const String googleApiKey = "AIzaSyAo215TRl_Nkdp1t0m48C6rda_c9vRD_E4";
+  // static const String googleApiKey = "AIzaSyAo215TRl_Nkdp1t0m48C6rda_c9vRD_E4";
+
+  static const String googleApiKey = "AIzaSyDyw9Lf_ffIhVAaCt2IA7Bx13ddLBdsbkg";
 }
